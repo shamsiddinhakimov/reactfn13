b7d2668f9bb019227b099d01d16a46812aa04580
faxriddinanvarjonov <faxriddinanvarjonov@gmail.com>
2024-01-27 02:06:55 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./components/header/Header";
+import MainBody from "./components/main/MainBody";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <Header />
+        <MainBody />
+      </div>
+    </>
+  );
+}
+
+export default App;
