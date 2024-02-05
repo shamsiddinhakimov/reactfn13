7b49d49134b927fa5b1b24a8643bb610a00a29aa
shamsiddinhakimov <shamsiddinhakimov@gmail.com>
2024-02-05 18:26:44 +0500

    dewqwqwq

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,68 @@
+import { useState, useRef, useEffect } from "react";
 import "./App.css";
-import Header from "./components/header/Header";
-import MainBody from "./components/main/MainBody";
 
 function App() {
+  let [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("https://auth-rg69.onrender.com/api/products/all")
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  function handleDelete(id) {
+    fetch(`https://auth-rg69.onrender.com/api/products/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((dataRes) => {
+        if (dataRes.message == "Mahsulot muvaffaqiyatli o'chirildi") {
+          let copied = JSON.parse(JSON.stringify(data));
+          copied = copied.filter((el) => {
+            return el.id != id;
+          });
+          console.log(copied);
+          setData(copied);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   return (
     <>
-      <div className="container">
-        <Header />
-        <MainBody />
-      </div>
+      <table className="rounded-xl">
+        <tr>
+          <th>No</th>
+          <th>Nomi</th>
+          <th>Narhi</th>
+          <th>Izoh</th>
+          <th>Amallar</th>
+        </tr>
+        {data.map((phone, index) => {
+          return (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{phone.name}</td>
+              <td>{phone.price}</td>
+              <td>{phone.description}</td>
+              <td>
+                <button
+                  className="bg-red-600 p-2 rounded-xl text-white"
+                  onClick={() => {
+                    handleDelete(phone.id);
+                  }}
+                >
+                  delete
+                </button>
+              </td>
+            </tr>
+          );
+        })}
+      </table>
     </>
   );
 }
